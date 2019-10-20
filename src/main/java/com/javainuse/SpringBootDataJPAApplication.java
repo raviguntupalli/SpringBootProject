df5d3628897147f0a2e0b0fb946ce2deb7420d20
raviguntupalli <56778173+raviguntupalli@users.noreply.github.com>
2019-10-19 22:15:36 -0400

    Add files via upload

---
+++ src/main/java/com/javainuse/SpringBootDataJPAApplication.java
@@ -0,0 +1,16 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootDataJPAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJPAApplication.class, args);
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/allEmployees.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<h1>Employees page</h1>
+
+<ul>
+<c:forEach items="" var="employee">
+   <li></li>
+</c:forEach>
+</ul>
